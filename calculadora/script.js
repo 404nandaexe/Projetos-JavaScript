3dcf4f8b739f2ae44662f74daa771b269af374c5
Fernanda Andrade <siahtokkmi@gmail.com>
2026-06-01 00:00:43 -0300

    atualização do script

---
+++ calculadora/script.js
@@ -0,0 +1,19 @@
+// Selecionando elementos HTML
+
+// Display da calculadora
+const result = window.document.getElementById('result')
+
+// Botôes numéricos
+const numbers = window.document.getElementsByClassName('number')
+
+// Botôes operadores
+const operators = window.document.getElementsByClassName('operator')
+
+// Botôes especiais
+const equal = window.document.getElementById('equal')
+const clear = window.document.getElementById('clear')
+const backspace = window.document.getElementById('backpace')
+
+// Estado da calculadora
+
+// Número que está 
